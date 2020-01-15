7ad35e2f79cfc2f02128ab9fe47f7efd73278ab2
Andriy Cherevatenko <achereva@softserveinc.com>
2020-01-15 17:20:22 +0200

    fix Page component - undo some refactoring

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -2,9 +2,8 @@ import React, { memo } from 'react';
 import './Page.less';
 import { useSelector } from 'react-redux';
 
-export const getArticle = (state) => {state.get('article')}; 
 const Page = memo(() => {
-    const article = useSelector(getArticle);
+    const article = useSelector((state) => state.get('article'));
     return <div className="page">{article}</div>;
 });
 
