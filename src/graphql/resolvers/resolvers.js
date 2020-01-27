0596172f286077721dece3f31afc03da2877f5fa
Andriy Cherevatenko <achereva@softserveinc.com>
2020-01-27 11:41:33 +0200

    removed unused queries and mutations

--- src/graphql/resolvers/resolvers.js
+++ src/graphql/resolvers/resolvers.js
@@ -4,13 +4,8 @@ const resolvers = {
     Query: {
         article: () => store.article,
         selectedCards: () => store.selectedCards,
-        articleSelectedCards: () => store.articleSelectedCards,
     },
     Mutation: {
-        updateArticle: (_, { value }) => {
-            store.article.value = value;
-            return store.article;
-        },
         updateCards: (_, { cards, article }) => {
             store.selectedCards.value = cards;
             store.article.value = article;
